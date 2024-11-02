5d4a024d4bfc3f2036e94caaee265421070445d1
Emii-15 <emilianoledesma250@gmail.com>
2024-11-02 04:50:25 -0300

    Mutantes.java

--- Examen_mercadolibre_mutantes/src/main/java/com/examen/mutantes/Mutantes.java
+++ Examen_mercadolibre_mutantes/src/main/java/com/examen/mutantes/Mutantes.java
@@ -3,7 +3,7 @@ package com.examen.mutantes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example")
+@SpringBootApplication(scanBasePackages = "com.examen.mutantes")
 public class Mutantes {
 	public static void main(String[] args) {
 		SpringApplication.run(Mutantes.class, args);
